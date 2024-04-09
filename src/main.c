b1028bf37b267ffeccae9216d7c7ab1b1bdda48b
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-04-09 15:07:46 +0100

    src/main.c : program structure defined

--- src/main.c
+++ src/main.c
@@ -12,10 +12,16 @@
 
 #include "push_swap.h"
 
-/*	Set flag to signal if alloced memory needs freeing;
- *	Increment argv to skip program name;
- *	Assign argv to input_list;
- *
+/*	PUSH_SWAP
+ *	- Set flag to signal if alloced memory needs freeing;
+ *	- Increment argv to skip program name;
+ *	- Assign argv to input_list;
+ *	- Check for errors;
+ *		- If there is an error free allocated memory;,
+ *	- Create stack_a and stack_b;
+ *	- if stack_a is not sorted
+ *		- sort it
+ *	- free allocated memory
  * */
 int	main(int argc, char **argv)
 {
@@ -25,9 +31,21 @@ int	main(int argc, char **argv)
 	int		must_free;
 	int		error;
 
-	must_free = 0;
 	++argv;
 	input_list = argv;
-
+	must_free = 0;
+	if (argc == 2)
+		input_list = ft_get_elems(&argc, argv, &must_free);
+	error = ft_errors(argc, input_list);
+	if (error <= 0)
+	{
+		ft_free_list(NULL, NULL, input_list, must_free);
+		return (0);
+	}
+	stack_a = ft_create_stack(argc, input_list, 1);
+	stack_b = ft_create_stack(argc, input_list, 0);
+	if (ft_is_sorted(stack_a) == -1)
+		ft_sort(stack_a, stack_b, argc);
+	ft_free_list(stack_a, stack_b, input_list, must_free);
 	return (0);
 }
