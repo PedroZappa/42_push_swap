2321d290204641adf30d06e2b0a8b6244705530c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-04-09 15:43:19 +0100

    src/ft_stack.c : ft_is_sorted() prototyped

--- src/ft_stack.c
+++ src/ft_stack.c
@@ -6,13 +6,18 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 15:33:19 by passunca          #+#    #+#             */
-/*   Updated: 2024/04/09 15:35:49 by passunca         ###   ########.fr       */
+/*   Updated: 2024/04/09 15:42:06 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 t_elem	*ft_create_stack(int argc, char **argv, int stack)
+{
+	return (0);
+}
+
+int	ft_is_sorted(t_elem *stack)
 {
 	return (0);
 }
