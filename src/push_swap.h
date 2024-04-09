95468c40e3b2aa8581ed9be9bb1278ba6d3bad74
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-04-09 15:39:24 +0100

    src/ft_errors.c : ft_errors() prototyped

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 21:01:22 by passunca          #+#    #+#             */
-/*   Updated: 2024/04/09 15:35:23 by passunca         ###   ########.fr       */
+/*   Updated: 2024/04/09 15:37:25 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,8 @@ typedef struct s_elem {
 
 /* ft_stack.c */
 t_elem	*ft_create_stack(int argc, char **argv, int stack);
+
+/* ft_errors.c */
+
 
 #endif
