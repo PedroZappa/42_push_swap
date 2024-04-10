8041b7a82492cd4dc67df3b74e4435c187202c83
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-04-10 16:37:07 +0100

    ft_errors : ft_are_args_nbr() implemented

--- src/ft_errors.c
+++ src/ft_errors.c
@@ -12,21 +12,52 @@
 
 #include "push_swap.h"
 
+static int	ft_are_args_nbr(int argc, char **argv);
+static int	ft_is_duplicate(int argc, char **argv);
+
 int	ft_errors(int argc, char **argv)
 {
 	(void) argc;
 	(void) argv;
-	// if (argc == 1)
-	// 	return (0);
-	// if ()
-	// {
-	// 	
-	// 	return (-1);
-	// }
-	// if ()
-	// {
-	//
-	// 	return (-1);
-	// }
+	if (argc == 1)
+		return (0);
+	if (ft_are_args_nbr(argc, argv) == -1)
+	{
+		ft_putstr_fd("Error\n", 1);
+		return (-1);
+	}
+	if (ft_is_duplicate(argc, argv) == -1)
+	{
+		ft_putstr_fd("Error\n", 1);
+		return (-1);
+	}
 	return (1);
+}
+
+static int	ft_are_args_nbr(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < (argc - 1))
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (ft_isdigit(argv[i][j]) == 0)
+				if ((j != 0) && (argv[i][j] != '-'))
+					return (-1);
+			++j;
+		}
+		++i;
+	}
+	return (1);
+}
+
+static int	ft_is_duplicate(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+	return (0);
 }
