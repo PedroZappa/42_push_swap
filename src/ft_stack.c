b58ac4e12a3fc485250196484bf39a19891b3ec3
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-04-11 12:42:16 +0100

    src/ft_stack.c : ft_create_stack() implemented & commented

--- src/ft_stack.c
+++ src/ft_stack.c
@@ -12,12 +12,46 @@
 
 #include "push_swap.h"
 
-t_elem	*ft_create_stack(int argc, char **argv, int stack)
+/*	ft_create_stack()
+ *		- Allocated memory for stack (+1 for sentinel value);
+ *		- If memory allocation fails return NULL;
+ *		- Loops over args excluding the last one;
+ *			- If select == 1 (building stack_a)
+ *				- Convert string to int;
+ *				- Set stack element as filled;
+ *			- If select == 0 (building stack_b)
+ *				- Set stack element as empty;
+ *			- Assign current stack element index;
+ *			- Increment stack index;
+ *		- After the loop set the index of sentinel value to -1;
+ *		- Return stack;
+ * */
+t_elem	*ft_create_stack(int argc, char **argv, int select)
 {
-	(void) argc;
-	(void) argv;
-	(void) stack;
-	return (0);
+	t_elem	*stack;
+	int		i;
+
+	stack = malloc(sizeof(t_elem) * (argc + 1));
+	if (stack == NULL)
+		return (NULL);
+	i = 0;
+	while (i < (argc - 1))
+	{
+		if (select == 1)
+		{
+			stack[i].num = ft_atoi(argv[i]);
+			stack[i].filled = 1;
+		}
+		else
+		{
+			stack[i].num = 0;
+			stack[i].filled = 0;
+		}
+		stack[i].index = i;
+		++i;
+	}
+	stack[i].index = -1;
+	return (stack);
 }
 
 int	ft_is_sorted(t_elem *stack)
