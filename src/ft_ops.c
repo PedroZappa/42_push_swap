e83c90ad7fbe2bd9e1f804c7d3c41518c2200f1d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-04-12 11:01:36 +0100

    src/ft_ops.c : ft_swap_elem() implemented

--- src/ft_ops.c
+++ src/ft_ops.c
@@ -6,14 +6,28 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 09:54:54 by passunca          #+#    #+#             */
-/*   Updated: 2024/04/12 09:58:02 by passunca         ###   ########.fr       */
+/*   Updated: 2024/04/12 11:01:08 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/*	ft_swap_elem()
+ *	- get the start of the stack (skipping sentinel value);
+ *	- store the first element in a temp variable;
+ *	- Perform the swap:
+ *		- set the first element to the next element;
+ *		- set the next element to the temp variable;
+ *	- Print the operation message to stdout;
+ * */
 void	ft_swap_elem(t_elem *stack, char *msg)
 {
-	(void) stack;
-	(void) msg;
+	int	start;
+	int	temp;
+
+	start = ft_get_stack_start(stack);
+	temp = stack[start].num;
+	stack[start].num = stack[(start + 1)].num;
+	stack[(start + 1)].num = temp;
+	ft_putstr_fd(msg, 1);
 }
