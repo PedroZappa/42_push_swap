7d8a0fb1e59142a737c7158a9149d6e45f8773da
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-04-12 10:55:23 +0100

    src/push_swap.h : ft_get_stack_start() impplemented

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 21:01:22 by passunca          #+#    #+#             */
-/*   Updated: 2024/04/09 15:42:49 by passunca         ###   ########.fr       */
+/*   Updated: 2024/04/12 10:31:35 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ typedef struct s_elem {
 /* ft_stack.c */
 t_elem	*ft_create_stack(int argc, char **argv, int stack);
 int		ft_is_sorted(t_elem *stack);
+int		ft_get_stack_start(t_elem *stack);
 
 /* ft_errors.c */
 int		ft_errors(int argc, char **argv);
