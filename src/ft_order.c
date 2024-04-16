18eb97ec0fc14d0258b9d83e9382a9fc0f430455
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-04-16 16:35:12 +0100

    ft_order.c : helper function prototyped

--- src/ft_order.c
+++ src/ft_order.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 15:46:10 by passunca          #+#    #+#             */
-/*   Updated: 2024/04/16 16:19:28 by passunca         ###   ########.fr       */
+/*   Updated: 2024/04/16 16:33:50 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_check_order(t_elem *stack_a, t_elem *stack_b, int idx)
 	int min;
 
 	n_op_a = get_op_for_topplace(stack_a, idx, 1);
-	if ((ft_get_stack_min(stack_b).num > stack_a[idx].num)) || \
+	if ((ft_get_stack_min(stack_b).num > stack_a[idx].num) || \
 		(ft_get_stack_max(stack_b, -1).num < stack_a[idx].num))
 		n_op_b = get_op_for_topplace(stack_b, ft_get_stack_min(stack_b).index, 1);
 	else
@@ -42,4 +42,12 @@ void	ft_order(t_elem *stack_a, t_elem *stack_b, int idx)
 	(void) stack_b;
 	(void) idx;
 	return ;
+}
+
+int	get_op_for_topplace(t_elem *stack_a, int idx, int sign)
+{
+	(void) stack_a;
+	(void) idx;
+	(void) sign;
+	return (0);
 }
