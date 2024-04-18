9c085baf43408342a5cd661bc601ab1d393f2339
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-04-18 11:47:19 +0100

    src/push_swap.h : function prototypes sorted by file name alphabetically

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 21:01:22 by passunca          #+#    #+#             */
-/*   Updated: 2024/04/18 09:16:15 by passunca         ###   ########.fr       */
+/*   Updated: 2024/04/18 11:46:54 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,19 +42,21 @@ typedef struct s_elem {
 /* ft_create_stack.c */
 t_elem	*ft_create_stack(int argc, char **argv, int stack);
 
-/* ft_stack.c */
-int		ft_is_sorted(t_elem *stack);
-int		ft_get_stack_start(t_elem *stack);
-int		ft_get_stack_end(t_elem *stack);
-t_elem	ft_get_stack_min(t_elem *stack);
-t_elem	ft_get_stack_max(t_elem *stack, int threshold);
-
 /* ft_errors.c */
 int		ft_errors(int argc, char **argv);
+
+/* ft_median.c */
+int		ft_get_median(t_elem *stack);
+t_elem	ft_min_above_thresh(t_elem *stack, int threshold);
 
 /* ft_ops.c */
 void	ft_swap_elem(t_elem *stack, char *msg);
 void	ft_push_elem(t_elem *stack_a, t_elem *stack_b, char *msg);
+
+/* ft_order.c */
+int		ft_check_order(t_elem *stack_a, t_elem *stack_b, int idx);
+void	ft_order(t_elem *stack_a, t_elem *stack_b, int idx);
+int		get_op_for_topplace(t_elem *stack_a, int idx, int sign);
 
 /* ft_rotate.c */
 void	ft_rotate(t_elem *stack, char *msg);
@@ -62,19 +64,17 @@ void	ft_rev_rotate(t_elem *stack, char *msg);
 int		ft_rotate_top(t_elem *stack_a, int index, char *r, char *rr);
 void	ft_rotate_both(t_elem *stack_a, t_elem *stack_b, int direction);
 
-/* ft_median.c */
-int		ft_get_median(t_elem *stack);
-t_elem	ft_min_above_thresh(t_elem *stack, int threshold);
+/* ft_sort_stack.c */
+void	ft_sort_stack(t_elem *stack_a, t_elem *stack_b, int stack_len);
 
 /* ft_sort_three.c */
 void	ft_sort_three(t_elem *stack);
 
-/* ft_sort_stack.c */
-void	ft_sort_stack(t_elem *stack_a, t_elem *stack_b, int stack_len);
-
-/* ft_order.c */
-int		ft_check_order(t_elem *stack_a, t_elem *stack_b, int idx);
-void	ft_order(t_elem *stack_a, t_elem *stack_b, int idx);
-int		get_op_for_topplace(t_elem *stack_a, int idx, int sign);
+/* ft_stack.c */
+int		ft_is_sorted(t_elem *stack);
+int		ft_get_stack_start(t_elem *stack);
+int		ft_get_stack_end(t_elem *stack);
+t_elem	ft_get_stack_min(t_elem *stack);
+t_elem	ft_get_stack_max(t_elem *stack, int threshold);
 
 #endif
