3bf72c9f6263d7cde5431f13fa9835b59adcee08
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-04-18 08:57:22 +0100

    src/ft_order.c : helper function implemented & commented

--- src/ft_order.c
+++ src/ft_order.c
@@ -44,10 +44,42 @@ void	ft_order(t_elem *stack_a, t_elem *stack_b, int idx)
 	return ;
 }
 
-int	get_op_for_topplace(t_elem *stack_a, int idx, int sign)
+/* get_op_for_topplace() (using rotations)
+ *	## Calculates the number of operations needed to move an element at a given
+ *	index to the top of a stack.
+ *
+ *	- Initialize n_ops counter to 0;
+ *	- Get the start of the stack;
+ *	- Get the end of the stack;
+ *	- Get the median position of the stack;
+ *	- If the index is greater than the median:
+ *		(closer to the end of the stack):
+ *		- Get the difference between the end of the stack and the index + 1;
+ *		- If the parameter sign is set to one:
+ *			- Make n_ops negative to signify the direction of rotation;
+ *	- Else if the index is less than or equal to the median 
+ *		(closer to the start of the stack):
+ *		- Get the difference between the index and the start of the stack;
+ *	- Return n_ops;	
+ * */
+int	get_op_for_topplace(t_elem *stack, int idx, int sign)
 {
-	(void) stack_a;
-	(void) idx;
-	(void) sign;
-	return (0);
+	int		n_ops;
+	int		start;
+	int		end;
+	int		median;
+
+	n_ops = 0;
+	start = ft_get_stack_start(stack);
+	end = ft_get_stack_end(stack);
+	median = (start + ((end - start) / 2));
+	if (idx > median)
+	{
+		n_ops = (end - idx + 1);
+		if (sign == 1)
+			n_ops = -n_ops;
+	}
+	else if (idx <= median)
+		n_ops = (idx - start);
+	return (n_ops);
 }
